cb5e716c676d65c0801c1e8d4b32d82ba513a5fb
joshy-joy <joshyjoy999@gmail.com>
2024-07-14 23:33:18 +0530

    intro added

--- src/components/main/sections/Home.vue
+++ src/components/main/sections/Home.vue
@@ -5,23 +5,35 @@ export default {
 
 <template>
     <div class="row h-100">
-        <div class="col-md-5 personal-details-wrap">
-            <div class="line-number-wrap">
-                <ul class="line-number">
-                    <li class="line-number-value" v-for="index in 30" :key="index">{{ index }}</li>
-                </ul>
+        <div class="col-sm-12">
+            <div class="personal-details-wrap">
+                <div class="line-number-wrap">
+                    <ul class="line-number">
+                        <li class="line-number-value" v-for="index in 30" :key="index">{{ index }}</li>
+                    </ul>
+                </div>
+                <div class="personal-details">
+                    <p class="intro">Hi all. I am</p>
+                    <h1 class="name">Joshy Joy</h1>
+                    <h4 class="profession code-blue">> Software Development Engineer</h4>
+                    <p class="overview code-comments">
+                        // He is passioned fro crafting innovative solutions and
+                    </p>
+                    <p class="overview code-comments">
+                        // pushing boundaries of technology
+                    </p>
+                    <p class="github-url">
+                        <span class="const code-blue">const</span>
+                        <span class="code-green">githublink</span> = 
+                        <a class="link" target="_blank" href="https://github.com/joshy-joy">
+                            <span class="code-orange">"https://github.com/joshy-joy"</span>
+                        </a>
+                    </p>
+                </div>
             </div>
-            <div class="personal-details">
-                <p class="intro">Hi all. I am</p>
-                <h1 class="name">Joshy Joy</h1>
-                <h4 class="profession code-blue">> Software Development Engineer</h4>
-                <p class="overview code-comments">
-                    // He is passioned fro crafting innovative solutions and pushing boundaries of technology
-                </p>
+            <div class="home-code-snippet">
+                
             </div>
-        </div>
-        <div class="col-md-6">
-            
         </div>
     </div>
 </template>
@@ -37,18 +49,19 @@ export default {
     padding: 0 15px 0 15px;
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
-    align-items: flex-end;
+    justify-content: flex-start;
+    align-items: center;
     list-style: none;
 }
 
 .line-number-value {
     text-decoration: none;
+    color: #6f7681;
 }
 
 .personal-details-wrap {
     display: flex;
-    justify-content: flex-end;
+    justify-content: flex-start;
     align-items: center;
 }
 
@@ -82,6 +95,14 @@ font-style: normal;
 font-weight: 450;
 font-size: 16px;
 line-height: 21px;
+}
+
+.github-url {
+ color: #E5E9F0;
+}
+
+.const {
+    padding: 0 10px 0 0;
 }
 
 </style>
