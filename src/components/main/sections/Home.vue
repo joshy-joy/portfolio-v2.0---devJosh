95ba1b29e2588e492c09b63447c9cad51897d061
joshy-joy <joshyjoy999@gmail.com>
2024-07-22 18:49:56 +0530

    home page contect added

--- src/components/main/sections/Home.vue
+++ src/components/main/sections/Home.vue
@@ -1,46 +1,65 @@
 <script lang="ts">
+
+import HomeCodeBlock from "./HomeCodeBlock.vue";
+
 export default {
+    components : {
+        'code-block': HomeCodeBlock
+    }
 }; 
+
 </script>
 
 <template>
     <div class="row h-100">
         <div class="col-sm-12">
-            <div class="personal-details-wrap">
-                <div class="line-number-wrap">
-                    <ul class="line-number">
-                        <li class="line-number-value" v-for="index in 30" :key="index">{{ index }}</li>
-                    </ul>
-                </div>
-                <div class="personal-details">
-                    <p class="intro">Hi all. I am</p>
-                    <h1 class="name">Joshy Joy</h1>
-                    <h4 class="profession code-blue">> Software Development Engineer</h4>
-                    <p class="overview code-comments">
-                        // He is passioned fro crafting innovative solutions and
-                    </p>
-                    <p class="overview code-comments">
-                        // pushing boundaries of technology
-                    </p>
-                    <p class="github-url">
-                        <span class="const code-blue">const</span>
-                        <span class="code-green">githublink</span> = 
-                        <a class="link" target="_blank" href="https://github.com/joshy-joy">
-                            <span class="code-orange">"https://github.com/joshy-joy"</span>
-                        </a>
-                    </p>
-                </div>
+            <div class="line-number-wrap">
+                <ul class="line-number">
+                    <li class="line-number-value" v-for="index in 30" :key="index">{{ index }}</li>
+                </ul>
             </div>
-            <div class="home-code-snippet">
-                
+            <div class="home-content-wrap">
+                <div class="personal-details-wrap">
+                    <div class="personal-details">
+                        <p class="intro">Hi all. I am</p>
+                        <h1 class="name">Joshy Joy</h1>
+                        <h4 class="profession code-blue">> Software Development Engineer II</h4>
+                        <p class="overview code-comments">
+                            // He is passioned for crafting innovative solutions and
+                        </p>
+                        <p class="overview code-comments">
+                            // pushing boundaries of technology
+                        </p>
+                        <p class="github-url">
+                            <span class="const code-blue">const</span>
+                            <span class="code-green">githublink</span> = 
+                            <a class="link" target="_blank" href="https://github.com/joshy-joy">
+                                <span class="code-orange">"https://github.com/joshy-joy"</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div class="code-snippet-wrap">
+                    <div class="code-block block-fade code-starter">
+                        <code-block></code-block>
+                    </div>
+                    <div class="code-block">
+                        <code-block></code-block>
+                    </div>
+                    <div class="code-block block-fade">
+                        <code-block></code-block>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-.col-md-1 {
-    padding: 0 !important;
+.col-sm-12 {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
 }
 
 .line-number {
@@ -59,10 +78,19 @@ export default {
     color: #6f7681;
 }
 
+.home-content-wrap {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    margin: 0 0 0 30px;
+}
+
 .personal-details-wrap {
     display: flex;
     justify-content: flex-start;
     align-items: center;
+    max-width: 500px;
+    margin: 0 20px 0 0;
 }
 
 .intro {
@@ -76,33 +104,55 @@ export default {
 }
 
 .name {
-font-style: normal;
-font-weight: 300;
-font-size: 62px;
-line-height: 81px;
-color: #E5E9F0;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 62px;
+    line-height: 81px;
+    color: #E5E9F0;
 }
 
 .profession {
-font-style: normal;
-font-weight: 450;
-font-size: 32px;
-line-height: 42px;
+    font-style: normal;
+    font-weight: 450;
+    font-size: 23px;
+    line-height: 42px;
 }
 
 .overview {
-font-style: normal;
-font-weight: 450;
-font-size: 16px;
-line-height: 21px;
+    font-style: normal;
+    font-weight: 450;
+    font-size: 13px;
+    line-height: 21px;
 }
 
 .github-url {
- color: #E5E9F0;
+    color: #E5E9F0;
 }
 
 .const {
     padding: 0 10px 0 0;
+}
+
+.code-snippet-wrap {
+    height: 650px;
+    overflow: hidden;
+    margin: 0 0 0 20px;
+}
+
+.code-block {
+    background: #001221;
+    padding: 20px;
+    margin: 10px 0 10px 0;
+    border-radius: 10px;
+    opacity: 70%;
+}
+
+.block-fade {
+    opacity: 30%;
+}
+
+.code-starter {
+    margin-top: -50px;
 }
 
 </style>
